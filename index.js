6657ca4377bb687a16073b657fb8fc47daf82071
duanhaochen <duanhaochen@126.com>
2017-09-22 23:01:59 +0800

    change

--- index.js
+++ index.js
@@ -6,33 +6,28 @@ var PluginError = gutil.PluginError;
 var PLUGIN_NAME = 'gulp-uglifyes';
 
 function gulpUglifyes(option){
+
   var stream = through.obj(function(file, enc, cb){
     if(file.isStream()){
       this.emit('error', new PluginError(PLUGIN_NAME, 'Streams are not supported!'));
       return cb();
     }
-
     if(file.isBuffer()){
       try{
-
         var str = file.contents.toString('utf8');
-
         var result = uglify.minify({
           'script': str
         }, option);
 
         file.contents = new Buffer(result.code);
-
         return cb(null, file);
-
       }catch(err){
-
         this.emit('error', new PluginError(PLUGIN_NAME, err));
-
         return cb();
       }
     }
   });
+
   return stream;
 }
 
