c05c70c284ac925af88f8d9207d27334466e9d85
段昊辰 <duanhaochen@126.com>
2020-08-02 23:12:45 +0800

    fix: 修复terserv5的terser.minify函数由同步调整为异步导致的错误 #14

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 /* @flow */
+import util from 'util';
 import through2 from 'through2';
 import terser from 'terser';
 import PluginError from 'plugin-error';
 import applySourceMap from 'vinyl-sourcemaps-apply';
 
 const PLUGIN_NAME: string = 'terser';
+
+type TerserResult = {
+  code?: string,
+  map?: string,
+  error?: any
+};
 
 /**
  * @param { Object } defaultOption: gulp传递的配置
@@ -39,25 +46,38 @@ function gulpTerser(defaultOption: Object = {}): Function{
           build = str;
         }
 
-        // 压缩代码
-        const result: Object = terser.minify(build, option);
+        // 压缩代码（terser5是异步，terser4是同步）
+        const resultPromise: Promise<TerserResult> | TerserResult = terser.minify(build, option);
 
-        // 输出报错信息
-        if('error' in result){
-          throw new Error(result.error.message);
+        // 编译函数
+        const setContents: Function = (result: TerserResult): void=>{
+          // 输出报错信息
+          if('error' in result){
+            throw new Error(result.error.message);
+          }
+
+          // Buffer
+          file.contents = 'from' in Buffer ? Buffer.from(result.code) : new Buffer(result.code);
+
+          // 输出source-map
+          if(file.sourceMap && result.map){
+            applySourceMap(file, result.map);
+          }
+
+          this.push(file);
+        };
+
+        if(util.types.isPromise(resultPromise)){
+          return resultPromise.then((result: TerserResult): void=>{
+            setContents(result);
+
+            return Promise.resolve(callback());
+          });
+        } else {
+          setContents(resultPromise);
+
+          return callback();
         }
-
-        // Buffer
-        file.contents = 'from' in Buffer ? Buffer.from(result.code) : new Buffer(result.code);
-
-        // 输出source-map
-        if(file.sourceMap && result.map){
-          applySourceMap(file, result.map);
-        }
-
-        this.push(file);
-
-        return callback();
       }catch(err){
         this.emit('error', new PluginError(PLUGIN_NAME, err));
         return callback();
